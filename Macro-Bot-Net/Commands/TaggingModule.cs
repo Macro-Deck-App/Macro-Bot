0b38e8cb017a5dec9e645ac7cf8d50854ec55a6a
Xenox003 <xenox003yt@gmail.com>
2022-07-19 20:12:43 +0200

    Added Handler for Modals

--- Macro-Bot-Net/Commands/TaggingModule.cs
+++ Macro-Bot-Net/Commands/TaggingModule.cs
@@ -1,4 +1,5 @@
-﻿using Develeon64.MacroBot.Services;
+﻿using Develeon64.MacroBot.Logging;
+using Develeon64.MacroBot.Services;
 using Discord;
 using Discord.Interactions;
 using Discord.WebSocket;
@@ -9,41 +10,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Develeon64.MacroBot.Commands
+namespace Develeon64.MacroBot.Commands.Tagging
 {
     [Group("tag", "Tag system")]
-    public class TaggingModule : InteractionModuleBase<SocketInteractionContext>
+    public class TaggingCommandsModule : InteractionModuleBase<SocketInteractionContext>
     {
 
-        [SlashCommand("create","Create a new tag")]
+        public static List<UserTagAssignable> createTagAssignments = new();
+        [SlashCommand("create", "Create a new tag")]
         public async Task Create([Summary(description: "Name of the tag")] string name)
         {
-            /*
+            // Handle Permissions
             SocketGuildUser guildUser = Context.Guild.GetUser(Context.User.Id);
-            String permisisonGroup = "PermissionManageTags";
-            if (!checkPermissions(permisisonGroup, guildUser)) {
-                EmbedBuilder errorEmbed = new()
-                {
-                    Title = "Invalid Permissions",
-                    Description = "You do not have the needed permissions to run this command!"
-                };
+            ulong[] requiredPermissions = ConfigManager.CommandsConfig.Tagging.PermissionManageTags;
+            if (!checkPermissions(requiredPermissions, guildUser)) {
 
-                String permissionList = "";
-                foreach (JValue permissionID in config.getObject(permisisonGroup).ToObject<JArray>()) permissionList += $"\n<@&{permissionID.ToString()}>";
-                errorEmbed.AddField("Required Roles", $"At least one of those roles is required:{permissionList}");
-
-                await RespondAsync(embed: errorEmbed.Build(),ephemeral: true);
+                await RespondAsync(embed: buildPermissionError(requiredPermissions), ephemeral: true);
                 return;
             }
-            */
 
+            // Insert user into Assignments cache to get tag name when the Modal is beeing sent
+            UserTagAssignable? assignable = createTagAssignments.Find(x => x.guildId == Context.Guild.Id && x.userId == Context.User.Id);
+            if (assignable != null)
+            {
+                createTagAssignments.Remove(assignable);
+            }
+            createTagAssignments.Add(new UserTagAssignable(Context.Guild.Id, Context.User.Id, name));
+
+            // Send Modal Prompt
             ModalBuilder modal = new ModalBuilder()
                 .WithTitle("Create Tag")
                 .WithCustomId("create_tag_modal")
                 .AddTextInput("Tag Name", "tag_name", TextInputStyle.Short, "Enter tag name", required: true)
                 .AddTextInput("Tag Content", "tag_content", TextInputStyle.Paragraph, "Enter tag content here", required: true);
 
-            await RespondWithModalAsync(modal.Build());
+            await RespondWithModalAsync<TagCreateModal>("tag_create_modal");
         }
 
         [SlashCommand("delete", "Delete an existing tag")]
@@ -61,16 +62,17 @@ namespace Develeon64.MacroBot.Commands
         [SlashCommand("view", "View a tag")]
         public async Task View()
         {
-            await RespondAsync("WIP");
+            EmbedBuilder embed = new EmbedBuilder()
+            {
+                Title = ""
+            };
         }
 
-        private static Boolean checkPermissions(string permissionGroup,SocketGuildUser user)
+        private static Boolean checkPermissions(ulong[] permissions, SocketGuildUser user)
         {
-            /*
-            JArray manageTagsPermissions = config.getObject(permissionGroup).ToObject<JArray>();
             List<SocketRole> userRoles = user.Roles.ToList<SocketRole>();
-            
-            foreach (JValue permission in manageTagsPermissions)
+
+            foreach (ulong permission in permissions)
             {
                 if (userRoles.Find(x => x.Id.ToString() == permission.ToString()) != null)
                 {
@@ -78,16 +80,79 @@ namespace Develeon64.MacroBot.Commands
                 }
             }
             return false;
-            */
-            return true;
         }
 
-        [ModalInteraction("create_tag_modal")]
-        public async Task HandleCreateModal(Modal modal)
+        private static Embed buildPermissionError(ulong[] permissions)
         {
-            Console.WriteLine(modal);
-            
-            await RespondAsync("test");
+            EmbedBuilder embedBuilder = new EmbedBuilder()
+            {
+                Title = "Invalid Permissions",
+                Description = "You do not meet the permission requirements to run this command!"
+            };
+            String permissionList = "";
+            foreach (ulong permissionID in permissions) permissionList += $"\n<@&{permissionID.ToString()}>";
+            embedBuilder.AddField("Required Roles", $"At least one of those roles is required:{permissionList}");
+            embedBuilder.WithColor(new Color(255, 50, 50));
+
+            return embedBuilder.Build();
+        }
+    }
+
+    public class TaggingInteractionModule : InteractionModuleBase<SocketInteractionContext>
+    {
+        [ModalInteraction("tag_create_modal")]
+        public async Task HandleCreateModal(TagCreateModal modal)
+        {
+            UserTagAssignable? assignable = TaggingCommandsModule.createTagAssignments.Find(x => x.guildId == Context.Guild.Id && x.userId == Context.User.Id);
+            if (assignable != null)
+            {
+                EmbedBuilder embedBuilder = new EmbedBuilder()
+                {
+                    Title = "Tag Created",
+                    Description = $"Successfully created tag `{assignable.tagName}`"
+                };
+                embedBuilder.AddField("Tag Content", modal.TagContent);
+                embedBuilder.WithColor(new Color(50, 255, 50));
+
+                TaggingCommandsModule.createTagAssignments.Remove(assignable);
+
+                await RespondAsync(embed: embedBuilder.Build());
+            } else
+            {
+                Logger.Warning(Modules.Tags, $"Could not find Tag name information for user {Context.User.Username} ({Context.User.Id}) in guild {Context.Guild.Name} ({Context.Guild.Id})!");
+                await RespondAsync("An internal error occured while getting Tag Name information, please try again.", ephemeral: true);
+            }
+        }
+    }
+
+    public class TagCreateModal : IModal
+    {
+        public string Title => "Create Tag";
+        [InputLabel("Tag Content")]
+        [ModalTextInput("tag_create_content", TextInputStyle.Paragraph, minLength: 1, maxLength: 4000)]
+        public string TagContent { get; set; }
+    }
+
+    public class TagEditModal : IModal
+    {
+        public string Title => "Edit Tag";
+        [InputLabel("Tag Content")]
+        [ModalTextInput("tag_create_content", TextInputStyle.Paragraph, minLength: 1, maxLength: 4000, placeholder: "Sadly we cannot provide the existing tag content here automatically due to Discord limitations")]
+        public string TagContent { get; set; }
+    }
+
+    public class UserTagAssignable
+    {
+        // Apparently i have not found a more efficient way in asigning the tag name to an discord modal so i needed to temporarly save this assignment into a List
+
+        public ulong userId { get; private set; }
+        public ulong guildId { get; private set; }
+        public string tagName{ get; private set; }
+        public UserTagAssignable(ulong guildID,ulong userID,string tagName)
+        {
+            this.userId = userID;
+            this.guildId = guildID;
+            this.tagName = tagName;
         }
     }
 }
