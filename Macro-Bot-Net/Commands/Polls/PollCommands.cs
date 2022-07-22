99f316618e86aebf0b88c220a83381265d512bdc
Xenox003 <xenox003yt@gmail.com>
2022-07-22 23:17:11 +0200

    Added Poll Create Command

---
+++ Macro-Bot-Net/Commands/Polls/PollCommands.cs
@@ -0,0 +1,35 @@
+﻿using Develeon64.MacroBot.Models;
+using Develeon64.MacroBot.Services;
+using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+
+namespace Develeon64.MacroBot.Commands.Polls
+{
+    [Group("poll", "Poll System")]
+    public class PollCommands : InteractionModuleBase<SocketInteractionContext>
+    {
+        public static List<PollChannelAssignable> createAssignables = new();
+
+        [SlashCommand("create", "Create a new poll")]
+        public async Task Create([Summary(description: "The Answer options for this poll")] PollOption option ,[Summary("channel","the channel to post the poll in")] ITextChannel? channel = null)
+        {
+            // Handle Permissions
+            SocketGuildUser guildUser = Context.Guild.GetUser(Context.User.Id);
+            ulong[] requiredPermissions = ConfigManager.CommandsConfig.Polls.PermissionManagePolls;
+            if (!PollUtils.checkPermissions(requiredPermissions, guildUser))
+            {
+
+                await RespondAsync(embed: PollUtils.buildPermissionError(requiredPermissions), ephemeral: true);
+                return;
+            }
+
+            PollChannelAssignable assignable = createAssignables.Find(x => x.user == Context.User.Id);
+            if (assignable != null) createAssignables.Remove(assignable);
+            createAssignables.Add(new PollChannelAssignable(Context.User.Id, option, channel));
+
+            // Reply with Modal
+            await RespondWithModalAsync<PollCreateModal>("poll_create_modal");
+        }
+    }
+}
