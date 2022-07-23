08291283b9e1ec68f0e48fe89b6735c39da31104
Xenox003 <xenox003yt@gmail.com>
2022-07-23 15:20:17 +0200

    Added Poll Object

---
+++ Macro-Bot-Net/Models/Poll.cs
@@ -0,0 +1,17 @@
+﻿namespace Develeon64.MacroBot.Models {
+	public class Poll {
+		public long Id { get; set; }
+		public ulong Author { get; set; }
+		public ulong MessageId { get; set; }
+		public ulong ChannelId { get; set; }
+		public ulong GuildId { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public bool Closed { get; set; }
+
+		public int Votes1 { get; set; }
+		public int Votes2 { get; set; }
+		public int Votes3 { get; set; }
+		public int Votes4 { get; set; }
+	}
+}
