0bf1feee506a841c2ceb78e6fc1b592a906ab295
Manuel Mayer <suchbyte@gmail.com>
2023-04-27 12:07:29 +0200

    Add buymeacoffee webhook

---
+++ MacroBotApp/Controllers/BuyMeACoffeeController.cs
@@ -0,0 +1,100 @@
+using MacroBot.Authentication;
+using MacroBot.Config;
+using MacroBot.Models.BuyMeACoffee;
+using MacroBot.Models.Webhook;
+using MacroBot.ServiceInterfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MacroBot.Controllers;
+
+[ApiController]
+[Route("/buymeaocoffee")]
+public class BuyMeACoffeeController : ControllerBase
+{
+    private readonly IDiscordService _discordService;
+    private readonly WebhooksConfig _webhooksConfig;
+
+    public BuyMeACoffeeController(IDiscordService discordService, WebhooksConfig webhooksConfig)
+    {
+        _discordService = discordService;
+        _webhooksConfig = webhooksConfig;
+    }
+
+    [HttpGet("/donationcreated")]
+    public async Task<IActionResult> DonationCreated([FromQuery] BuyMeACoffeeDonationCreatedRequest donationCreatedRequest)
+    {
+        if (donationCreatedRequest.Data?.Succeeded is false)
+        {
+            return BadRequest();
+        }
+        var webhook = _webhooksConfig.Webhooks.FirstOrDefault(x => x.Id.Equals("buymeacoffee"));
+        if (webhook == null)
+        {
+            return NotFound();
+        }
+        
+        var authResult = Request.CheckAuthentication(webhook);
+
+        switch (authResult)
+        {
+            case AuthenticationResult.Unauthorized:
+                return StatusCode(StatusCodes.Status401Unauthorized);
+            case AuthenticationResult.Forbidden: 
+                return StatusCode(StatusCodes.Status403Forbidden);
+        }
+
+        var supporterName = donationCreatedRequest.Data?.SupporterName ?? "anonymous";
+        var amount = donationCreatedRequest.Data?.Amount?.ToString("#0.00") ?? "hidden";
+        var currency = donationCreatedRequest.Data?.Currency ?? string.Empty;
+        var message = donationCreatedRequest.Data?.SupportNote;
+
+        var webHookRequest = new WebhookRequest
+        {
+            Embed = new WebhookRequestEmbed
+            {
+                Color = new WebhookRequestEmbedColor
+                {
+                    R = 0,
+                    G = 1,
+                    B = 0
+                },
+                Title = "New donation",
+                Fields = new List<WebhookRequestEmbedField>()
+                {
+                    new ()
+                    {
+                        Name = "Donator",
+                        Value = supporterName,
+                        Inline = true
+                    },
+                    new ()
+                    {
+                        Name = "Amount",
+                        Value = $"{amount} {currency}",
+                        Inline = true
+                    }
+                }
+            }
+        };
+
+        if (!string.IsNullOrWhiteSpace(message))
+        {
+            webHookRequest.Embed.Fields.Add(new WebhookRequestEmbedField
+            {
+                Name = "Message",
+                Value = message,
+                Inline = false
+            });
+        }
+        
+        webHookRequest.Embed.Fields.Add(new WebhookRequestEmbedField
+        {
+            Name = "\nAlso want to donate to the project?",
+            Value = "https://buymeacoffee.com/suchbyte",
+            Inline = false
+        });
+
+        await _discordService.BroadcastWebhookAsync(webhook, webHookRequest);
+        return Ok();
+    }
+}
---
+++ MacroBotApp/Models/BuyMeACoffee/BuyMeACoffeeDonationCreatedData.cs
@@ -0,0 +1,26 @@
+using System.Text.Json.Serialization;
+
+namespace MacroBot.Models.BuyMeACoffee;
+
+public class BuyMeACoffeeDonationCreatedData
+{
+    [JsonPropertyName("amount")]
+    public float? Amount { get; set; }
+    
+    [JsonPropertyName("status")]
+    public string? Status { get; set; }
+    
+    [JsonPropertyName("currency")]
+    public string? Currency { get; set; }
+    
+    [JsonPropertyName("note_hidden")]
+    public bool NoteHidden {get; set; }
+    
+    [JsonPropertyName("support_note")]
+    public string? SupportNote { get; set; }
+    
+    [JsonPropertyName("supporter_name")]
+    public string? SupporterName { get; set; }
+
+    public bool Succeeded => Status?.Equals("succeeded", StringComparison.InvariantCultureIgnoreCase) ?? false;
+}
