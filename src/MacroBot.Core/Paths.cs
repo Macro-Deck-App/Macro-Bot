def422c1a147cfcc5dac8eaeab658ac5c7e48a55
Manuel Mayer <45011179+manuelmayer-dev@users.noreply.github.com>
2023-08-08 22:59:47 +0200

    Develop (#84)
    * Remove command logging
    
    * Update config path

--- src/MacroBot.Core/Paths.cs
+++ src/MacroBot.Core/Paths.cs
@@ -2,29 +2,18 @@ namespace MacroBot.Core;
 
 public static class Paths
 {
-    private static string MainDirectory
-    {
-        get
-        {
-            var environment = Environment.GetEnvironmentVariable("ENVIRONMENT");
-            if (string.IsNullOrWhiteSpace(environment))
-            {
-                return "__testConfigs__";
-            }
-            return environment == "DEVELOPMENT" ? "/etc/macro bot dev" : "/etc/macro bot";
-        }
-    }
-
-    public static readonly string WebhooksPath = Path.Combine(MainDirectory, "Webhooks.json");
-    public static readonly string BotConfigPath = Path.Combine(MainDirectory, "BotConfig.json");
-    public static readonly string CommandsConfigPath = Path.Combine(MainDirectory, "Commands.json");
-    public static readonly string StatusCheckConfigPath = Path.Combine(MainDirectory, "StatusCheck.json");
-    public static readonly string DatabasePath = Path.Combine(MainDirectory, "Database.db");
-    public static readonly string KoFiConfigPath = Path.Combine(MainDirectory, "KoFi.json");
+    private static string ConfigDirectory => "Config";
+    
+    public static readonly string WebhooksPath = Path.Combine(ConfigDirectory, "Webhooks.json");
+    public static readonly string BotConfigPath = Path.Combine(ConfigDirectory, "BotConfig.json");
+    public static readonly string CommandsConfigPath = Path.Combine(ConfigDirectory, "Commands.json");
+    public static readonly string StatusCheckConfigPath = Path.Combine(ConfigDirectory, "StatusCheck.json");
+    public static readonly string DatabasePath = Path.Combine(ConfigDirectory, "Database.db");
+    public static readonly string KoFiConfigPath = Path.Combine(ConfigDirectory, "KoFi.json");
 
     public static void EnsureDirectoriesCreated()
     {
-        CheckAndCreateDirectory(MainDirectory);
+        CheckAndCreateDirectory(ConfigDirectory);
         static void CheckAndCreateDirectory(string path)
         {
             if (Directory.Exists(path))
